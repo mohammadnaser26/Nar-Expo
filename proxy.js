1e212f8b8d4e59d4a70d2b89c9ac45885d0d9a71
adhamoq <adhamalmoqdad@gmail.com>
2025-11-05 21:06:12 +0100

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -18,7 +18,8 @@ const corsOptions = {
       'http://localhost:3000',
       'http://127.0.0.1:3000',
       'http://localhost:5500',
-      'http://localhost:8000'
+      'http://localhost:8000',
+      'https://narvsexp.site'
     ];
     
     // Check if the origin is in the allowed list or is a GitHub Pages subdomain
